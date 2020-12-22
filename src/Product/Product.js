092079fa309a8e1781a59259087500a611a9ac19
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-21 22:46:53 -0500

    add div around info & change out of stock text to red

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -2,19 +2,21 @@ import React from 'react';
 import './Product.css';
 
 export default function Product(props) {
-  const { title, price, availability, description, id } = props.product;
+  const { title, price, availability, description } = props.product;
   const { addToCart } = props;
 
   return (
     <div className="Product">
       <h2>{title}</h2>
-      <span>${price}</span>
-      <h4>Description:</h4>
-      <p>{description}</p>
+      <div className="Product__info">
+        <span>${price}</span>
+        <h4>Description:</h4>
+        <p>{description}</p>
+      </div>
       {
         availability === "in_stock" ?
           <button onClick={() => addToCart(props.product)}>Add To Cart</button> :
-          <span>Out of Stock</span>
+          <span className="out-of-stock">Out of Stock</span>
       }
     </div>
   );
