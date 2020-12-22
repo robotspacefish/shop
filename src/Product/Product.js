d4c30f29bf5bc8d9f296deba1b861b66abdc6b25
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-21 21:46:17 -0500

    display add to cart or out of stock for each product

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Product.css';
 
 export default function Product({ title, price, availability, description }) {
   return (
     <div className="Product">
       <h2>{title}</h2>
-      <span>{price}</span>
+      <span>${price}</span>
       <h4>Description:</h4>
       <p>{description}</p>
+      {
+        availability === "in_stock" ?
+          <button>Add To Cart</button> :
+          <span>Out of Stock</span>
+      }
     </div>
   );
 }
