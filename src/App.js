bbd4dafde5478437c7903744fdcde10488f750f0
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-21 21:50:50 -0500

    move state to main app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
+import React, { Component } from 'react';
+import Products from './Products/Products';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const URL = 'https://spreadsheets.google.com/feeds/list/1Cp0owZ_71huZOBLiX57hKTvxKYEo4qZC1y_IAHV6rX4/od6/public/values?alt=json';
+
+class App extends Component {
+  state = {
+    products: [],
+    isLoading: true
+  };
+
+  fetchProducts() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        this.setupProducts(data.feed.entry);
+      })
+      .catch(e => console.log(e))
+  }
+
+  setupProducts(data) {
+    const products = data.map((p, i) => (
+      {
+        title: p.title.$t,
+        price: p.gsx$priceincents.$t,
+        availability: p.gsx$availability.$t,
+        description: p.gsx$description.$t,
+        id: i
+      }
+    ))
+
+    this.setState(() => ({ products, isLoading: false }));
+  }
+
+  componentDidMount() {
+    console.log('fetching')
+    this.fetchProducts();
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <a href="#">Cart</a>
+
+        {
+          this.state.isLoading ?
+            "Loading Products..." : <Products products={this.state.products} />
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,57 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Product from '../Product/Product';
+import './Products.css';
 
-const URL = 'https://spreadsheets.google.com/feeds/list/1Cp0owZ_71huZOBLiX57hKTvxKYEo4qZC1y_IAHV6rX4/od6/public/values?alt=json';
+export default function Products(props) {
+  const { products } = props;
 
-export default class Products extends Component {
-  state = {
-    products: [],
-    isLoading: true
-  };
-
-  fetchProducts() {
-    fetch(URL)
-      .then(res => res.json())
-      .then(data => {
-        this.setupProducts(data.feed.entry);
-      })
-      .catch(e => console.log(e))
-  }
-
-  setupProducts(data) {
-    const products = data.map((p, i) => (
-      {
-        title: p.title.$t,
-        price: p.gsx$priceincents.$t,
-        availability: p.gsx$availability.$t,
-        description: p.gsx$description.$t,
-        id: i
-      }
-    ))
-
-    this.setState(() => ({ products, isLoading: false }));
-  }
-
-  componentDidMount() {
-    console.log('fetching')
-    this.fetchProducts();
-
-  }
-
-  renderProducts() {
-    return this.state.products.map(p => (
+  const renderProducts = () => (
+    products.map(p => (
       <Product key={p.id} {...p} />
     ))
-  }
+  );
 
-  render() {
-    console.log('render')
-    console.log(this.state.products)
-    return (
-      <div className="Products">
-        <h1>Products</h1>
-        {this.state.isLoading ? "Loading Products..." : this.renderProducts()}
+  return (
+    <div className="Products">
+      <h1>Products</h1>
+      <div className="Products__content">
+        {renderProducts()}
       </div>
-    );
-  }
+    </div>
+  );
 }
