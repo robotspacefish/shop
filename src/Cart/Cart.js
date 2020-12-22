2e46aaf95994afa7583d0b19ca9f63ddfefd1c76
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-21 22:55:15 -0500

    display text for empty cart

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -2,19 +2,25 @@ import React from 'react';
 import CartItem from '../CartItem/CartItem';
 
 export default function Cart(props) {
+  const renderCart = () => {
+    return props.cart.map(p => (
+      <CartItem key={p.id} product={p} removeFromCart={props.removeFromCart} />
+    ))
+  };
+
   return (
     <div className="cart">
       <h1>Cart</h1>
-      <div className="Cart__content">
-        {
-          props.cart.map(p => (
-            <CartItem key={p.id} product={p} removeFromCart={props.removeFromCart} />
-          ))
-        }
-      </div>
+      <h4>Total: <span>${props.cartTotal}</span></h4>
 
       <hr />
-      <h4>Total: <span>${props.cartTotal}</span></h4>
+
+      <div className="Cart__content">
+        {
+          props.cart.length === 0 ?
+            <h3>Your Cart is Empty!</h3> : renderCart()
+        }
+      </div>
     </div>
   );
 }
